98514d830d1dd4672f338e0e54544018e57c098b
Michele Palmieri <palmierimichele561@gmail.com>
2026-01-26 09:37:02 +0100

    Improve appointment start/end widgets

--- core/forms.py
+++ core/forms.py
@@ -27,11 +27,30 @@ class PropertyForm(forms.ModelForm):
         fields = "__all__"
 
 
+from django import forms
+from .models import Appointment
+
 class AppointmentForm(forms.ModelForm):
     class Meta:
         model = Appointment
         fields = "__all__"
+        widgets = {
+            "start": forms.DateTimeInput(attrs={"type": "datetime-local"}),
+            "end": forms.DateTimeInput(attrs={"type": "datetime-local"}),
+        }
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # se in futuro Django cambia widget, lo forziamo qui
+        self.fields["start"].widget.input_type = "datetime-local"
+        self.fields["end"].widget.input_type = "datetime-local"
+
+        # Precompila correttamente in edit
+        for f in ("start", "end"):
+            dt = getattr(self.instance, f, None)
+            if dt:
+                self.initial[f] = dt.strftime("%Y-%m-%dT%H:%M")
 
 class AgentForm(forms.ModelForm):
     class Meta:
